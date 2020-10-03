69b6ab93667ff02838dad3ec7464392c725d3665
Giovanni Dante Grazioli <giovanni.dantegrazioli@nbs-system.com>
2020-10-03 20:38:54 +0200

    added swedish

--- generate-data.py
+++ generate-data.py
@@ -12,6 +12,7 @@ import base64
 
 WORKI_DIR  = os.path.dirname(os.path.realpath(__file__))
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; rv:25.0) Gecko/20100101 Firefox/25.0'
+IS_DEBUG   = False
 
 APIS = {
 	"en": {
@@ -21,6 +22,7 @@ APIS = {
 		"domapi": "https://southpark.cc.com",
 		"uri": "/seasons/south-park/",
 		"html_links": False,
+		"has_ads": False,
 	},
 	"es": {
 		"language": "es",
@@ -29,6 +31,7 @@ APIS = {
 		"domapi": "https://southpark.cc.com",
 		"uri": "/es/seasons/south-park/",
 		"html_links": False,
+		"has_ads": True,
 	},
 	"de": {
 		"language": "de",
@@ -37,6 +40,7 @@ APIS = {
 		"domapi": "https://southpark.de",
 		"uri": "/seasons/south-park/",
 		"html_links": True,
+		"has_ads": False,
 	},
 	"se": {
 		"language": "se",
@@ -45,8 +49,17 @@ APIS = {
 		"domapi": "https://southparkstudios.nu",
 		"uri": "/seasons/south-park/",
 		"html_links": False,
+		"has_ads": False,
 	}
 }
+
+def log_debug(msg):
+	if IS_DEBUG:
+		print("[D] {}".format(msg))
+
+def log_struct(data):
+	if IS_DEBUG:
+		print(json.dumps(data, indent=4))
 
 def _http_get(url, is_json=False):
 	if len(url) < 1:
@@ -59,8 +72,13 @@ def _http_get(url, is_json=False):
 	data = data.decode("utf-8")
 	if is_json:
 		data = json.loads(data, strict=False)
-	#print("http get: {0}".format(url))
+	log_debug("http get: {0}".format(url))
 	return data
+
+def write_data(path, data):
+	with open(path,'w') as output:
+		output.truncate()
+		output.write(data)
 
 def write_json(path, data):
 	with open(path, 'w') as fp:
@@ -81,22 +99,24 @@ def _dk(obj, keys, default=None):
 					found = o
 					break
 			if found == None:
-				#log_debug("not found: {}".format(k))
+				log_debug("not found: {}".format(k))
 				return default
 			obj = found
 		elif isinstance(obj, dict) and k not in obj:
-			#log_debug("not found: {}".format(k))
+			log_debug("not found: {}".format(k))
 			return default
 		elif isinstance(obj, list) and isinstance(k, int) and k >= len(obj):
-			#log_debug("not found: {}".format(k))
+			log_debug("not found: {}".format(k))
 			return default
 		else:
 			obj = obj[k]
 	return obj
 
 def _make_episode(data, season, episode, lang):
+	has_ads  = APIS[lang]["has_ads"]
 	domapi   = APIS[lang]["domapi"]
 	mediagen = APIS[lang]["mediagen"]
+
 	ep = {
 		"image":   _dk(data, ["media", "image", "url"], ""),
 		"uuid":    _dk(data, ["id"], ""),
@@ -108,28 +128,33 @@ def _make_episode(data, season, episode, lang):
 		"episode": "{}".format(episode + 1),
 		"mediagen": []
 	}
+
 	try:
 		args = "uri=mgid:arc:episode:{mediagen}:{uuid}&configtype=edge&ref={dom}{ref}".format(mediagen=mediagen, uuid=ep["uuid"], dom=domapi, ref=ep["url"])
 		url  = "https://media.mtvnservices.com/pmt/e1/access/index.html?{args}".format(args=args)
 		service = _http_get(url, True)
 		url = service["seamlessMediaGen"].replace('/{uri}/', "/" + service["uri"] + "/")
+		urls = []
 		try:
-			_http_get(url, True)
-			urls = [url]
+			if len(url) > 0:
+				_http_get(url, True)
+				urls = [url]
 		except HTTPError:
 			urls = []
-		if len(urls) < 1:
-			urls = _dk(service, ["feed", "items"], [])
+		if len(urls) < 1 or has_ads:
+			items = _dk(service, ["feed", "items"], [])
 			i = 0
-			for url in urls:
-				urls[i] = _dk(url, ["group", "content"], "").replace("&device={device}", "") + "&format=json&acceptMethods=hls"
+			for url in items:
+				items[i] = _dk(url, ["group", "content"], "").replace("&device={device}", "") + "&format=json&acceptMethods=hls"
 				i += 1
+			if len(items) > 0:
+				urls = items
 		ep["mediagen"] = urls
-	except HTTPError: # as e:
-		#print("http get: {0}".format(url), e)
-		pass
+	except HTTPError as e:
+		log_debug("http get: {0} {1}".format(url, e))
 
 	ep["mediagen"] = list(filter(None, ep["mediagen"]))
+	log_struct(ep)
 
 	print("s{:<2}e{:<2} len:{}: {}".format(ep["season"], ep["episode"], len(ep["mediagen"]), ep["title"]))
 	i = 0
@@ -145,12 +170,20 @@ def _has_extra(x):
 def _parse_episodes(data, season, lang):
 	domapi = APIS[lang]["domapi"]
 	print("parsing episodes from season", season + 1)
+	extra = []
 	lists = _dk(data,["children", "type|MainContainer", "children"], [])
-	lists = list(filter(lambda x: "type" in x and x["type"] == "LineList", lists))
+	if lang in ["en", "es"]:
+		lists = list(filter(lambda x: "type" in x and x["type"] == "LineList", lists))
+		extra = list(filter(lambda x: _has_extra(x), [ _dk(s, ["props"], []) for s in lists ]))
+		lists = list(filter(lambda x: len(x) > 0 and "url" in x[0], [ _dk(s, ["props", "items"], []) for s in lists ]))[0]
 
-	extra = list(filter(lambda x: _has_extra(x), [ _dk(s, ["props"], []) for s in lists ]))
+	elif lang == "se":
+		lists = list(filter(lambda x: "type" in x and x["type"] == "LineList" and "type" in x["props"] and x["props"]["type"] == "video-guide", lists))
+		lists = _dk(lists[0], ["props", "items"], [])
 
-	lists = list(filter(lambda x: len(x) > 0 and "url" in x[0], [ _dk(s, ["props", "items"], []) for s in lists ]))[0]
+	if len(lists) > 0 and _dk(lists[0], ["meta", "subHeader"], None) == None:
+		return []
+
 	lists = [_make_episode(lists[i], season, i, lang) for i in range(0, len(lists))]
 
 	if len(extra) > 0:
@@ -164,9 +197,9 @@ def _parse_episodes(data, season, lang):
 
 def _download_data(url, html_links):
 	webpage = _http_get(url)
-	#with open("/tmp/test.html",'wb') as output:
-	#	output.truncate()
-	#	output.write(webpage)
+	if IS_DEBUG:
+		write_data("debug-data.html", webpage)
+
 	if "window.__DATA__" in webpage:
 		dataidx  = webpage.index("window.__DATA__")
 		data     = webpage[dataidx:]
@@ -174,6 +207,10 @@ def _download_data(url, html_links):
 		equalidx = data.index("=")
 		data     = data[equalidx + 1:endidx + 1].strip()
 		data     = json.loads(data, strict=False)
+
+		if IS_DEBUG:
+			write_json("debug-data.json", data)
+
 		if html_links:
 			links = re.findall(r"href=\"/seasons/south-park/[\w]+/[\w]+-\d+", webpage, flags=re.M)
 			links = [x.split('"')[1] for x in links]
@@ -185,7 +222,6 @@ def generate_data(lang):
 	domain     = APIS[lang]["domain"]
 	uri        = APIS[lang]["uri"]
 	html_links = APIS[lang]["html_links"]
-
 
 	data = _download_data(domain + uri, html_links)
 	main = _dk(data,["children", "type|MainContainer", "children"])
@@ -203,7 +239,10 @@ def generate_data(lang):
 		if url == None:
 			continue
 		data = _download_data(domain + url, False)
-		seasons.append(_parse_episodes(data, len(seasons_urls) - index, lang))
+		lists = _parse_episodes(data, len(seasons_urls) - index, lang)
+		if len(lists) < 1 and len(seasons) < 1:
+			continue
+		seasons.append(lists)
 
 	seasons.reverse()
 	return {
@@ -216,7 +255,10 @@ def generate_file(lang):
 	write_json("addon-data-{}.json".format(lang), data)
 
 def main():
+	global IS_DEBUG
+
 	parser = argparse.ArgumentParser()
+	parser.add_argument('--debug', action='store_true', default=False, help='enable debug')
 	group = parser.add_mutually_exclusive_group(required=True)
 	group.add_argument('--en', action='store_true', default=False, help='language english')
 	group.add_argument('--es', action='store_true', default=False, help='language spanish')
@@ -225,6 +267,8 @@ def main():
 	args = parser.parse_args()
 
 	os.chdir(WORKI_DIR)
+
+	IS_DEBUG = args.debug
 
 	if args.en:
 		generate_file("en")
